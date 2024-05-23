9e9e8bbf186d3f6a399250ef5e60417329d27a9d
hoon <rissk12@naver.com>
2024-05-23 11:12:04 +0900

    required checkbox

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import emailjs from '@emailjs/browser'
 import { useRef } from 'react'
-import { Box, Text, Flex, Image, Textarea, Button } from '@chakra-ui/react'
+import { Box, Text, Flex, Image, Textarea, Button, Checkbox, Tooltip } from '@chakra-ui/react'
 import { CommonInput } from '@/components/CommonInput'
 const { useState } = require('react')
 
@@ -43,39 +43,44 @@ return (
           <CommonInput type="text" name="to_name" required defaultValue={'류연석'} hidden/>
           <CommonInput label={'이름'} type="text" name="from_name" required  placeholder='이름을 입력해주세요'/>
           <CommonInput label={'전화번호'} type="text" name="from_phone" required placeholder='연락받으실 전화번호를 입력해주세요!'/>
-            <Box pos='relative'>
-            <Text sx={{ fontsize:'14px', fontWeight:'600', pb:'6px' }}>견적문의 내용</Text>
-            <Textarea
-                required
-                name="message" 
-                minW='352px'
-                minH='240px'
-                placeholder='매장에 대한 문의사항을 자유롭게 작성해주세요. ex) 매장위치, 면적, 매출액 등등'
-                resize='none'
-                onChange={onTextareaHandler}
-                _focus={{ boxShadow: '0px 0px 0px 4px #195AFF3D' }}
-                maxLength={501}
-                isInvalid={isInvalid}
-                _invalid={{ boxShadow: '0px 0px 0px 1px #FDA29B' }}
-                py='12px'
-                sx={{
-                  '&::-webkit-scrollbar': {
-                    width: '8px',
-                  },
-                  '&::-webkit-scrollbar-track': {
-                    display: 'none',
-                  },
-                  '&::-webkit-scrollbar-thumb': {
-                    backgroundColor: 'gray',
-                    borderRadius: '6px',
-                  },
-            
-                }} />
-              <Box pos='absolute' right='14px' bottom='12px' sx={{ fontSize:'12px', color: 'gray' }}>
-                {inputCount}/500
-              </Box>
+          <Box pos='relative'>
+          <Text sx={{ fontsize:'14px', fontWeight:'600', pb:'6px' }}>견적문의 내용</Text>
+          <Textarea
+              required
+              name="message" 
+              minW='352px'
+              minH='240px'
+              placeholder='매장에 대한 문의사항을 자유롭게 작성해주세요. ex) 매장위치, 면적, 매출액 등등'
+              resize='none'
+              onChange={onTextareaHandler}
+              _focus={{ boxShadow: '0px 0px 0px 4px #195AFF3D' }}
+              maxLength={501}
+              isInvalid={isInvalid}
+              _invalid={{ boxShadow: '0px 0px 0px 1px #FDA29B' }}
+              py='12px'
+              sx={{
+                '&::-webkit-scrollbar': {
+                  width: '8px',
+                },
+                '&::-webkit-scrollbar-track': {
+                  display: 'none',
+                },
+                '&::-webkit-scrollbar-thumb': {
+                  backgroundColor: 'gray',
+                  borderRadius: '6px',
+                },
+          
+              }} />
+            <Box pos='absolute' right='14px' bottom='12px' sx={{ fontSize:'12px', color: 'gray' }}>
+              {inputCount}/500
             </Box>
+          </Box>
 
+          <Checkbox size='md' colorScheme='pink' name='from_checked' fontWeight={600} sx={{ fontSize:'12px'}} cursor={'pointer'} required>
+              <Tooltip label={agreeText} w={'100%'} p='12px 16px' borderRadius='8px' bg={'#0C111D'}>
+              [필수]개인정보 수집 및 이용 동의
+              </Tooltip>
+            </Checkbox>
           <Button bg={'#FFFFFF'} border={'1px solid #EAECF0'} _hover={{ bg: '#195AFF', color: '#FFFFFF'}} type="submit" w='100%' form='submit'>견적신청 하기</Button>
           </Flex>
         </form>
@@ -83,6 +88,14 @@ return (
     </Box>
     )
 }
+
+const agreeText = "주류어때 서비스 이용을 위해 아래의 개인정보 수집 및 이용에 동의해 주세요. \
+수집 항목: 이름, 연락처 \
+이용 목적: 서비스 제공 및 고객 문의 응대 \
+보유 기간: 서비스 이용 종료 후 1년까지 (관련 법령에 따라 필요한 경우, 해당 기간 동안 보관) \
+보관 방법: 이메일로만 수집, 별도의 데이터베이스에 저장되지 않음 \
+동의를 거부하실 경우, 서비스 이용이 제한될 수 있습니다."
+
 
 const SERVICE_ID = 'service_ipv23p9'
 const TEMPLATE_ID = 'template_fspvrml'
